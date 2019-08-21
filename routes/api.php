47fe514ee0f8bb4aaa57ce4555d56cabea9a1709
MatthieuJ <matthieu.jasselette@gmail.com>
2019-08-21 12:46:21 +0200

    prefix routes with v1

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('character', 'CharacterController');
+Route::prefix('v1')->group(function(){
+    Route::apiResource('character', 'CharacterController');
+});
