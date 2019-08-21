7825043fabe7a5107a9cba6a659d7cd4ac040dbe
MatthieuJ <matthieu.jasselette@gmail.com>
2019-08-21 15:47:22 +0200

    add & securise show & update user

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,6 @@ Route::prefix('v1')->group(function(){
     Route::post('/register', 'AuthController@register');
     Route::post('/login', 'AuthController@login');
     Route::post('/logout', 'AuthController@logout');
-    Route::resource('/user', 'AuthController')
+    Route::apiResource('/user', 'AuthController')
         ->only('show', 'update');
 });
