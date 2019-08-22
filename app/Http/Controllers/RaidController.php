810a3828d08d4889786d458f2e95adf72b7a5c46
MatthieuJ <matthieu.jasselette@gmail.com>
2019-08-22 11:18:37 +0200

    add successfull double condition on raid index controller

--- app/Http/Controllers/RaidController.php
+++ app/Http/Controllers/RaidController.php
@@ -4,18 +4,17 @@ namespace App\Http\Controllers;
 
 // use App\Character;
 use App\User;
-use App\Http\Resources\CharacterCollection;
+use App\Http\Resources\UserCollection;
 use Illuminate\Http\Request;
 
 class RaidController extends Controller
 {
-    public function index()
-    // public function index(): CharacterCollection
+    public function index(): UserCollection
     {
-        $user = User::where('is_available',1)->get();
+        $user = User::where('is_available',1)->where('main_char_id', "!=", NULL)->get();
         
         
-        return $user;
-        // return new CharacterCollection(Character::paginate());
+        return new UserCollection($user);
+
     }
 }
