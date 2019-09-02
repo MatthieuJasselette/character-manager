74431c9ca948b219dd6ba649fa719e5138cc45d0
MatthieuJ <matthieu.jasselette@gmail.com>
2019-09-02 14:48:36 +0200

    remove unused code

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,20 +14,6 @@ class UserResource extends JsonResource
      */
     public function toArray($request)
     {
-        // $characters = $this->character;
-        // $filteredCharacters = foreach ($characters as $character) {
-        //     return [
-        //     'id' => $this->character->id,
-        //     'name' => $this->character->name,
-        //     'description' => $this->character->description
-        //     ];
-        // }
-        $filteredImage = [
-            'id'  => $this->image->id,
-            'image_url'  => 'http://localhost:8000/thumbs/'.$this->image->name
-//  !important, change the endpoint to whatever adress the api is actually deployed
-        ];
-
         return [
             'id'            => $this->id,
             'name'          => $this->name,
@@ -35,7 +21,7 @@ class UserResource extends JsonResource
             'is_available'  => $this->is_available,
             'main_char_id'  => $this->main_char_id,
             'characters'    => $this->character,
-            'image'         => $filteredImage // seems to work
+            'image'         => $this->image
         ];
     }
 }
