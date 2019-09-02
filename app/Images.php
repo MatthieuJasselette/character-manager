cae8685cc95667fa007a94292f2545ca7ce024cc
MatthieuJ <matthieu.jasselette@gmail.com>
2019-09-02 11:17:00 +0200

    change id type to uuid & add relevant logic in the model

--- app/Images.php
+++ app/Images.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Images extends Model
 {
@@ -14,5 +15,24 @@ class Images extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($post) {
+            $post->{$post->getKeyName()} = (string) Str::uuid();
+        });
+    }
+
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    public function getKeyType()
+    {
+        return 'string';
     }
 }
--- database/migrations/2019_08_30_141954_create_images_table.php
+++ database/migrations/2019_08_30_141954_create_images_table.php
@@ -14,7 +14,7 @@ class CreateImagesTable extends Migration
     public function up()
     {
         Schema::create('images', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->uuid('id');
             $table->uuid('user_id');
             $table->string('name');
             $table->timestamps();
