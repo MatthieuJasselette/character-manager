dbe2ba39281ec1cb0dbbee6ee34332163da70106
MatthieuJ <matthieu.jasselette@gmail.com>
2019-09-02 13:04:54 +0200

    remove unnecessary code, probably will get thrown away

--- app/Http/Resources/ImagesResource.php
+++ app/Http/Resources/ImagesResource.php
@@ -14,9 +14,6 @@ class ImagesResource extends JsonResource
      */
     public function toArray($request)
     {
-        // return [
-        //     "image" => $this->media->getFullUrl() //fails; call function on null
-        // ];
         return [
             'id'        => $this->id,
             'name'      => $this->name,
