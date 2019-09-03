acd58f8cf619afef44c4bdb7fd73b942cb5838e7
MatthieuJ <matthieu.jasselette@gmail.com>
2019-09-03 10:51:04 +0200

    add default img on user creation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Image;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -10,11 +11,16 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $user = User::create([
-            'name'      => $request->name,
-            'email'     => $request->email,
-            'password'  => $request->password,
-            'is_available' => $request->is_available
+            'name'          => $request->name,
+            'email'         => $request->email,
+            'password'      => $request->password,
+            'is_available'  => $request->is_available
          ]);
+
+        $image = Image::create([
+            'name'      => 'http://localhost:8000/thumbs/default_logo.png',
+            'user_id'   => $user->id
+        ]);
 
         $token = auth()->login($user);
         $id = $user->id;
